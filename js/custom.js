4b2a56ecd2671ec553c2dd917c027f7e229d9d2e
root <praveenjm@eliteisinc.com>
2017-01-30 20:33:57 +0530

    Refactored the code

--- js/custom.js
+++ js/custom.js
@@ -1,408 +1,90 @@
+
 (function($) {
- 
-    // Init ScrollMagic
-    var ctrl = new ScrollMagic.Controller({
-        globalSceneOptions: {
-            triggerHook: 'onLeave'
-        }
-    });
-    $("#product_brief .slide").each(function() {
- 
-    new ScrollMagic.Scene({
-        triggerElement: this
-    })
-    .setPin(this)
- //    // .addIndicators({
- //    colorStart: "rgba(255,255,255,0.5)",
- //    colorEnd: "rgba(255,255,255,0.5)", 
- //    colorTrigger : "rgba(255,255,255,1)",
- //    name:name
-	// }) 
-	.loglevel(3)
-    .addTo(ctrl);
- 
-	});
- 
- var wh = window.innerHeight;
- 
-new ScrollMagic.Scene({
-  offset: wh*3
-})
-.setClassToggle("#container4", "is-active")
-.addTo(ctrl);
+	    var controller = new ScrollMagic.Controller();
+ var updateBox=function(e){
+ 	var $slideParentElement = $(e.currentTarget.triggerElement()),
+	 		 		$prevParentElement=$('.container.scroll_container',$slideParentElement).closest('.scrollmagic-pin-spacer').prev('.scrollmagic-pin-spacer'),
+	 		 		$nextParentElement=$('.container.scroll_container',$slideParentElement).closest('.scrollmagic-pin-spacer').next('.scrollmagic-pin-spacer'),
+	 		 		$slidePrevImg = $('.brief-img_in img' ,$prevParentElement),
+	 		 		$slidePrevContent = $('.desc_product' ,$prevParentElement),
+	 		 		$slideImg = $('.brief-img_in img', $slideParentElement),	 
+	 		 		$slideContent = $('.desc_product',$slideParentElement),
+	 		 		$slideNextImg = $('.brief-img_in img' ,$nextParentElement),
+	 		 		$slideNextContent = $('.desc_product' ,$nextParentElement);	
+console.log(e.type)
+				if(e.type==="enter"){
+					
+					$slideParentElement.removeClass('hide-div');
+			 		$('.container.scroll_container').addClass('zero-opacity');
+			 		$slideContent.addClass("fadeInDownBig");
+					$slideContent.removeClass("fadeOutDownBig");
+					$slideImg .addClass("zoomIn");
+					$slideImg .removeClass("zoomOut");
 
-// trans
-	var controller = new ScrollMagic.Controller();
-	var ww = $( window ).width();
-	// alert(ww);
+					$slidePrevContent.removeClass(" fadeInDownBig");
+					$slidePrevContent.addClass("fadeOutDownBig");
+					$slidePrevImg.removeClass("zoomIn");
+					$slidePrevImg.addClass("zoomOut");	
+			 		$('.container.scroll_container',e.currentTarget.triggerElement()).removeClass('zero-opacity');
+			 	}
+			 	if(e.type==="leave"){
+						$slideContent.removeClass("fadeInDownBig");
+						$slideContent.addClass("fadeOutDownBig");
+						$slideImg .removeClass("zoomIn");
+						$slideImg .addClass("zoomOut");
 
-	// $("#product_brief.slide").each(function(){
-	// 	var tween = TweenMax.from($this, 0.005, {autoAlpha:0,scale:0.5,y:'+=30', ease:Linear.easeNone});
-	// 	//build scene
-	// 	var scene=new ScrollMagic.Scene({
-	// 	triggerElement:this
-	// 	})
-	// 	.triggerHook(0)
-	// 	.setTween(tween)
-	// 	.addTo(controller);
+						$slidePrevContent.addClass("fadeInDownBig");
+						$slidePrevContent.removeClass("fadeOutDownBig");
+						$slidePrevImg.addClass("zoomIn");
+						$slidePrevImg.removeClass("zoomOut");	
+					 	$('.container.scroll_container',$prevParentElement).removeClass('zero-opacity');					 		
+			 	}
+	}
 
-
-	// });
-
-	// build scenes
-	var landPage = new ScrollMagic.Scene({triggerElement:'#landing-page'});
-					landPage.on('start', function(e){
-						$("#slide01").removeClass("show-div");
-						$("#slide01").addClass("hide-div");
-						$('#slide-nav').addClass('hide-nav');
-      					$('#slide-nav').removeClass('show-nav');
-					});
-					landPage.triggerHook(0);
-					if(ww > 319 && ww <= 415)
-					 {
-					 landPage.duration(350);
-					}
-					else{
-						landPage.duration(200);
-					}
-					landPage.triggerHook(0.7)
-					// landPage.addIndicators();
-					landPage.addTo(controller);
-	// var tweenSlide1 = new TimelineLite();
-	// 	tweenSlide1.from('#slide01', 0, {opacity:0})
-	var slider1 = new ScrollMagic.Scene({triggerElement: "#slide01"});
-					slider1.setClassToggle("#carsl1", "active-nav"); // add class toggle
-					slider1.on("enter", function(e){
-						// $('#slide-nav').addClass('show-nav');
-      // 					$('#slide-nav').removeClass('hide-nav');
-						$("#slide01").removeClass("hide-div");	
-						$("#slide01").addClass("show-div");
-						$("#content1-slide").addClass("fadeInDownBig");
-						$("#content1-slide").removeClass("fadeOutDownBig");
-						$("#img1-slide").addClass("zoomIn");
-						$("#img1-slide").removeClass("zoomOut");
-						// }else
-						// return;
-					// });
-					});
-					slider1.on("leave", function(){
-						$("#content1-slide").removeClass("fadeInDownBig");
-						$("#content1-slide").addClass("fadeOutDownBig");
-						$("#img1-slide").addClass("zoomOut");
-						$("#img1-slide").removeClass("zoomIn");
-					});
-					slider1.triggerHook(0);
-					 // slider1.addIndicators(); // add indicators (requires plugin)
-					 if(ww > 319 && ww <= 415)
-					 {
-					 slider1.duration(300);
-					}
-					else{
-						slider1.duration(600);
-					}
+	var someFunc=function(controller){
+		var slider1 = new ScrollMagic.Scene({triggerElement: "#slide01"});
+					// var tween1 = new TimelineLite();
+					// tween1.from("#img1-slide" ,0.4, {scale: 0});
+					// tween1.from("#content1-slide",0.4, {top: -700, opacity: 0},'-=0.4');
+					slider1.addIndicators(); 
+					slider1.setPin("#slide01");
+					slider1.on("enter leave",updateBox);
+					//slider1.setTween(tween1);
+					
 					slider1.addTo(controller);
 	var slider2 = new ScrollMagic.Scene({triggerElement: "#slide02"});
-					slider2.setClassToggle("#carsl2", "active-nav"); // add class toggle
-					slider2.on("enter", function(e){
-					// $("#slide02").waypoint(function(direction){
-					// 	if (direction === 'down')
-					// 	{
-						$("#content2-slide").addClass("fadeInDownBig");
-						$("#content2-slide").removeClass("fadeOutDownBig");
-						
-						$("#slide01").removeClass("show-div");
-						$("#slide02").removeClass("hide-div");
-						$("#slide02").addClass("show-div");
-						$("#img2-slide").addClass("zoomIn");
-						$("#img2-slide").removeClass("zoomOut");
-						// $("#content1-slide").removeClass("fadeInDownBig");
-						// $("#content1-slide").addClass("fadeOutDownBig");
-						// $("#img1-slide").addClass("zoomOut");
-						// $("#img1-slide").removeClass("zoomIn");
-					// 	}else
-					// 	return;
-					// });	
-
-					});
-					slider2.on("leave", function(){
-						$("#content2-slide").removeClass("fadeInDownBig");
-						$("#content2-slide").addClass("fadeOutDownBig");
-						$("#img2-slide").addClass("zoomOut");
-						$("#img2-slide").removeClass("zoomIn");
-					});
-					slider2.triggerHook(0);
-					if(ww > 319 && ww <= 415)
-					 {
-					 slider2.duration(200);
-					}
-					else{
-						slider2.duration(600);
-					}
-					// slider2.addIndicators(); // add indicators (requires plugin)
+					// var tween2 = new TimelineLite();
+					// tween2.from("#img2-slide" ,0.4, {scale: 0});
+					// tween2.from("#content2-slide",0.4, {top: -700, opacity: 0},'-=0.4');
+					slider2.addIndicators(); 
+					//slider2.setTween(tween2);
+					slider2.on("enter leave", updateBox);
+					//slider2.triggerHook(0.3);
+					slider2.setPin("#slide02");
 					slider2.addTo(controller);
 	var slider3 = new ScrollMagic.Scene({triggerElement: "#slide03"});
-					slider3.setClassToggle("#carsl3", "active-nav"); // add class toggle
-					slider3.on("enter", function(e){
-						// $("#slide03").waypoint(function(direction){
-						// if (direction === 'down')
-						// {
-						$("#content3-slide").addClass("fadeInDownBig");
-						$("#content3-slide").removeClass("fadeOutDownBig");
-						// $("#slide04").removeClass("show-div");
-						$("#slide02").removeClass("show-div");
-						$("#slide03").removeClass("hide-div");
-
-						$("#slide03").addClass("show-div");
-
-						$("#img3-slide").addClass("zoomIn");
-						$("#img3-slide").removeClass("zoomOut");
-						// $("#content2-slide").removeClass("fadeInDownBig");
-						// $("#content2-slide").addClass("fadeOutDownBig");
-						// $("#img2-slide").addClass("zoomOut");
-						// $("#img2-slide").removeClass("zoomIn");
-					// }
-					// else
-					// 	return;
-					// });
-						
-					});
-					slider3.on("leave", function(){
-						$("#content3-slide").removeClass("fadeInDownBig");
-						$("#content3-slide").addClass("fadeOutDownBig");
-						$("#img3-slide").addClass("zoomOut");
-						$("#img3-slide").removeClass("zoomIn");
-					});
+					// var tween3 = new TimelineLite();
+					// tween3.from("#img3-slide" ,0.4, {scale: 0});
 					
-					slider3.triggerHook(0);
-					if(ww > 319 && ww <= 415)
-					 {
-					 slider3.duration(200);
-					}
-					else{
-						slider3.duration(600);
-					}
-					// slider3.addIndicators(); // add indicators (requires plugin)
+					// tween3.from("#content3-slide", 0.4, {top: -700, opacity: 0},'-=0.4');
+					//slider3.setTween(tween3);
+					slider3.addIndicators(); 
+					// slider3.triggerHook(0.3);
+					slider3.on("enter leave", updateBox);
+					slider3.setPin("#slide03");
 					slider3.addTo(controller);
 	var slider4 = new ScrollMagic.Scene({triggerElement: "#slide04"});
-					slider4.setClassToggle("#carsl4", "active-nav"); // add class toggle
-					slider4.on("enter", function(e){
-						// $("#slide04").waypoint(function(direction){
-						// if (direction === 'down' || direction === 'up')
-						// {
-						// $("#content4-slide").removeClass("fadeOutDownBig");
-						$("#slide03").removeClass("show-div");
-						$("#slide04").removeClass("hide-div");
-						$("#slide04").addClass("show-div");
-						$("#img4-slide").removeClass("zoomOut");
-						$("#img4-slide").addClass("zoomIn");
-						$("#content4-slide").addClass("fadeInDownBig");
-						$("#content4-slide").removeClass("fadeOutDownBig");
-						// repeat
-						$("#content3-slide").removeClass("fadeInDownBig");
-						$("#content3-slide").addClass("fadeOutDownBig");
-						$("#img3-slide").addClass("zoomOut");
-						$("#img3-slide").removeClass("zoomIn");
-					});
-					slider4.on("leave", function(){
-						$("#content4-slide").removeClass("fadeInDownBig");
-						$("#content4-slide").addClass("fadeOutDownBig");
-						$("#img4-slide").addClass("zoomOut");
-						$("#img4-slide").removeClass("zoomIn");
-					});
+					// var tween4 = new TimelineLite();
+					// tween4.from("#img4-slide" ,0.4, {scale: 0});
 
-					slider4.triggerHook(0);
-					if(ww > 319 && ww <= 415)
-					 {
-					 slider4.duration(100);
-					}
-					else{
-						slider4.duration(600);
-					}
-					// slider4.addIndicators(); // add indicators (requires plugin)
-					// slider4.setPin('#slide4', {pushFollowers:false});
+					// tween4.from("#content4-slide", 0.4, {top: -700, opacity: 0},'-=0.4');
+					slider4.addIndicators(); 
+					//slider4.setTween(tween4);
+					//slider4.triggerHook(0.3);
+					slider4.on("enter leave", updateBox);
+					slider4.setPin("#slide04");
 					slider4.addTo(controller);
-	
-		var contact = new ScrollMagic.Scene({triggerElement: "#contact-form"});
-					contact.on("enter", function(e){
-					$('#side-nav').addClass('hide-nav');
-					$('#side-nav').removeClass('show-nav');
-
-					// $("#slide04").removeClass("show-div");
-					// $("#slide04").addClass("hide-div");
-					$('#contact-form').addClass('show-div');
-					$('#contact-form').removeClass('hide-div');
-				});
-					contact.duration(100);
-					contact.triggerHook(1);
-					// contact.addIndicators();
-					contact.addTo(controller);
-
-	// for img in-out
-	// var ctrl_anim= new ScrollMagic.Controller();
-	// // slide 1
-	// var slider1=new ScrollMagic.Scene({triggerElement: "#shrinkOut_img1", duration:600});
-	// 				slider1.on("start", function(e){
-	// 					$("#content1-slide").addClass("fadeInDownBig");
-	// 					$("#content1-slide").removeClass("fadeOutDownBig");
-	// 					$("#img1-slide").addClass("zoomIn");
-	// 					$("#img1-slide").removeClass("zoomOut");
-						
-	// 				});
-					
-	// 				// slider1.on("end", function(e){
-	// 				// 	// alert("a");
-	// 				// 	$("#content1-slide").removeClass("fadeInDownBig");
-	// 				// 	$("#content1-slide").addClass("fadeOutDownBig");
-	// 				// 	$("#img1-slide").addClass("zoomOut");
-	// 				// 	$("#img1-slide").removeClass("zoomIn");
-	// 				// });
-	// 				slider1.addIndicators(); // add indicators (requires plugin)
-	// 				slider1.addTo(ctrl_anim);
-
-
-
-	// var slider2=new ScrollMagic.Scene({triggerElement: "#shrinkOut_img2", duration:600});
-	// 				slider2.on("start", function(e){
-	// 					$("#content2-slide").addClass("fadeInDownBig");
-	// 					$("#content2-slide").removeClass("fadeOutDownBig");
-	// 					$("#img2-slide").addClass("zoomIn");
-	// 					$("#img2-slide").removeClass("zoomOut");
-	// 					$("#content1-slide").removeClass("fadeInDownBig");
-	// 					$("#content1-slide").addClass("fadeOutDownBig");
-	// 					$("#img1-slide").addClass("zoomOut");
-	// 					$("#img1-slide").removeClass("zoomIn");
-
-						
-	// 				});
-					
-	// 				// slider2.on("end", function(e){
-	// 				// 	// alert("a");
-	// 				// 	$("#content2-slide").removeClass("fadeInDownBig");
-	// 				// 	$("#content2-slide").addClass("fadeOutDownBig");
-	// 				// 	$("#img2-slide").addClass("zoomOut");
-	// 				// 	$("#img2-slide").removeClass("zoomIn");
-	// 				// });
-	// 				slider2.addIndicators(); // add indicators (requires plugin)
-	// 				slider2.addTo(ctrl_anim);
-
-
-
-	// var slider3=new ScrollMagic.Scene({triggerElement: "#shrinkOut_img3", duration:600});
-	// 				slider3.on("start", function(e){
-	// 					$("#content3-slide").addClass("fadeInDownBig");
-	// 					$("#content3-slide").removeClass("fadeOutDownBig");
-	// 					$("#img3-slide").addClass("zoomIn");
-	// 					$("#img3-slide").removeClass("zoomOut");
-	// 					$("#content2-slide").removeClass("fadeInDownBig");
-	// 					$("#content2-slide").addClass("fadeOutDownBig");
-	// 					$("#img2-slide").addClass("zoomOut");
-	// 					$("#img2-slide").removeClass("zoomIn");
-						
-	// 				});
-					
-	// 				// slider3.on("end", function(e){
-	// 				// 	// alert("a");
-	// 				// 	$("#content3-slide").removeClass("fadeInDownBig");
-	// 				// 	$("#content3-slide").addClass("fadeOutDownBig");
-	// 				// 	$("#img3-slide").addClass("zoomOut");
-	// 				// 	$("#img3-slide").removeClass("zoomIn");
-	// 				// });
-	// 				slider3.addIndicators(); // add indicators (requires plugin)
-	// 				slider3.addTo(ctrl_anim);
-
-
-	// var slider4=new ScrollMagic.Scene({triggerElement: "#shrinkOut_img4", duration:600});
-	// 				slider4.on("start", function(e){
-	// 					$("#content4-slide").addClass("fadeInDownBig");
-	// 					$("#content4-slide").removeClass("fadeOutDownBig");
-	// 					$("#img4-slide").addClass("zoomIn");
-	// 					$("#img4-slide").removeClass("zoomOut");
-	// 					$("#content3-slide").removeClass("fadeInDownBig");
-	// 					$("#content3-slide").addClass("fadeOutDownBig");
-	// 					$("#img3-slide").addClass("zoomOut");
-	// 					$("#img3-slide").removeClass("zoomIn");
-						
-	// 				});
-					
-	// 				// slider4.on("end", function(e){
-	// 				// 	// alert("a");
-	// 				// 	$("#content4-slide").removeClass("fadeInDownBig");
-	// 				// 	$("#content4-slide").addClass("fadeOutDownBig");
-	// 				// 	$("#img4-slide").addClass("zoomOut");
-	// 				// 	$("#img4-slide").removeClass("zoomIn");
-	// 				// });
-	// 				slider4.addIndicators(); // add indicators (requires plugin)
-	// 				slider4.addTo(ctrl_anim);
-	// new ScrollMagic.Scene({triggerElement: "#container2", duration:600})
-					
-	// 				.setClassToggle("#img2-slide", "zoomIn")
-					
-	// 				.addIndicators() // add indicators (requires plugin)
-	// 				.addTo(ctrl_anim);
-
-	// new ScrollMagic.Scene({triggerElement: "#container2", duration:600})
-					
-	// 				.setClassToggle("#content2-slide", "fadeInDownBig")
-					
-	// 				.addIndicators() // add indicators (requires plugin)
-	// 				.addTo(ctrl_anim);
-
-	// new ScrollMagic.Scene({triggerElement: "#shrinkOut_img2", duration:600})
-	// 				.setClassToggle("#img2-slide", "zoomOut")
-					
-	// 				.addIndicators() // add indicators (requires plugin)
-	// 				.addTo(ctrl_anim);
-
-	// new ScrollMagic.Scene({triggerElement: "#shrinkOut_img2", duration:600})
-					
-	// 				.setClassToggle("#content2-slide", "fadeOutDownBig")
-					
-	// 				.addIndicators() // add indicators (requires plugin)
-	// 				.addTo(ctrl_anim);
-
-	// new ScrollMagic.Scene({triggerElement: "#container3", duration:600})
-					
-	// 				.setClassToggle("#img3-slide", "zoomIn")
-					
-	// 				.addIndicators() // add indicators (requires plugin)
-	// 				.addTo(ctrl_anim);
-
-	// new ScrollMagic.Scene({triggerElement: "#container3", duration:600})
-					
-	// 				.setClassToggle("#content3-slide", "fadeInDownBig")
-	// 				.addIndicators() // add indicators (requires plugin)
-	// 				.addTo(ctrl_anim);
-
-	// new ScrollMagic.Scene({triggerElement: "#shrinkOut_img3", duration:600})
-	// 				.setClassToggle("#img3-slide", "zoomOut")
-					
-	// 				.addIndicators() // add indicators (requires plugin)
-	// 				.addTo(ctrl_anim);
-
-	// new ScrollMagic.Scene({triggerElement: "#shrinkOut_img3", duration:600})
-					
-	// 				.setClassToggle("#content3-slide", "fadeOutDownBig")
-					
-	// 				.addIndicators() // add indicators (requires plugin)
-	// 				.addTo(ctrl_anim);
-
-	// new ScrollMagic.Scene({triggerElement: "#container4", duration:600})
-					
-	// 				.setClassToggle("#img4-slide", "zoomIn")
-					
-	// 				.addIndicators() // add indicators (requires plugin)
-	// 				.addTo(ctrl_anim);
-
-	// new ScrollMagic.Scene({triggerElement: "#container4", duration:600})
-					
-	// 				.setClassToggle("#content4-slide", "fadeInDownBig")
-	// 				.addIndicators() // add indicators (requires plugin)
-	// 				.addTo(ctrl_anim);
-
-	
-
-
-	// // slide 2
-	
+	}
+		someFunc(controller);
 
 })(jQuery);
